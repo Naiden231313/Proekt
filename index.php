ff277ce376a2ca045fce7c7a0604ae081d05e832
Naiden231313 <22419@uktc-bg.com>
2026-01-12 12:17:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require 'db.php';
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->execute([$username, $password]);
+
+    echo "Регистрация успешна! <a href='login.php'>Log in</a>";
+}
+?>
+
+<form method="POST">
+    Username: <input name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button>Регистрация</button>
+</form>
