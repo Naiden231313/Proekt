f1a96364321ceef35cf26f24734a6caf3d8bbdaf
Naiden231313 <22419@uktc-bg.com>
2026-01-12 12:20:59 +0200

    Update register.php

--- register.php
+++ register.php
@@ -15,5 +15,4 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
 <form method="POST">
     Username: <input name="username" required><br>
     Password: <input type="password" name="password" required><br>
-    <button>Регистрация</button>
-</form>
+    <button>
